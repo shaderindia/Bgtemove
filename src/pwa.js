8af065d8d8124344c8ad8b2e341dc86423629659
Nishikant Xalxo <nishikantx08@gmail.com>
2025-11-04 18:02:25 +0200

    Change service worker registration path to relative
    Updated service worker registration to use a relative path.

--- src/pwa.js
+++ src/pwa.js
@@ -1,10 +1,11 @@
 /**
  * PWA service worker registration
+ * Use relative path so it works on GitHub Pages project sites
  */
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(reg => console.log('SW registered:', reg.scope))
-            .catch(err => console.log('SW registration failed:', err));
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(reg => console.log('SW registered:', reg.scope))
+      .catch(err => console.log('SW registration failed:', err));
+  });
 }
